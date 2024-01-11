2eb6d7793e12117671390186e3af138c3d9a0a3e
Larry W. <l422y@l422y.com>
2024-01-11 01:27:28 -0500

    Add new AwesomeComponent.vue file to basic components

---
+++ test/fixtures/basic/components/content/AwesomeComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="awesome--component">
+    <h1>Awesome Component</h1>
+    <p>This is an awesome component!</p>
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
