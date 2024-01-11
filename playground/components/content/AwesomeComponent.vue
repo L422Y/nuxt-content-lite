2b6c73261ecb26da84a269634e5808360108bfa8
Larry W. <l422y@l422y.com>
2024-01-10 20:44:54 -0500

    Add new AwesomeComponent with useContentLite composable

---
+++ playground/components/content/AwesomeComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="awesome--component">
+    <h1>Awesome Component</h1>
+    <p>This is an awesome component!</p>
+    {{ data }}
+  </div>
+</template>
+<script lang="ts" setup>
+import { useContentLite } from "../../../src/runtime/composables/useContentLite"
+
+const content = await useContentLite()
+const data = content.contentData
+</script>
