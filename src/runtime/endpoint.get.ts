b494965b7acbeab50f90571838e5b628f5d47257
Larry W. <l422y@l422y.com>
2024-01-11 02:08:11 -0500

    Refactor endpoint.get.ts to use defineEventHandler instead of eventHandler

--- src/runtime/endpoint.get.ts
+++ src/runtime/endpoint.get.ts
@@ -3,7 +3,7 @@ import * as fs from "fs"
 
 let json: any
 
-export default eventHandler(async () => {
+export default defineEventHandler(async () => {
     if (json === undefined) {
         const jsonPath = path.resolve("./", ".cache/content-lite/content-lite.json")
         json = fs.promises.readFile(jsonPath, "utf-8").then((data) => JSON.parse(data))
