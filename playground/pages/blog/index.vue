2d3d6dc71c2b401af72de06b111029bffbd9771b
Larry W. <l422y@l422y.com>
2024-01-10 20:45:30 -0500

    Add blog index page with post listing in Vue and TypeScript

---
+++ playground/pages/blog/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page blog">
+    <h1>Blog</h1>
+    <div class="blog--tiles">
+      <template
+        v-for="post in posts"
+        :key="post.title"
+      >
+        <NuxtLink :to="post.path" class="blog--tile">
+          <h2>
+            {{ post.title }}
+          </h2>
+          <time>
+            {{ new Date(post.date).toLocaleDateString() }}
+          </time>
+          <p>
+            {{ post.description }}
+          </p>
+        </NuxtLink>
+      </template>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const posts = ref()
+const content = await useContentLite({flattenData: true})
+posts.value = await content.routeContent()
+</script>
+<style lang="scss" scoped src="~/scss/blog.scss" />
