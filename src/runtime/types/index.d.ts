d49a6742cdce7b82fd6ca6901ec95965ee83cf20
Larry W. <l422y@l422y.com>
2024-01-10 20:46:29 -0500

    Add new interfaces and types for content handling in runtime/types

---
+++ src/runtime/types/index.d.ts
@@ -0,0 +1,48 @@
+export interface IContentLiteOptions {
+    /**
+     * Whether to return filterable content, which includes set of all unique words for each content item
+     */
+    filterable?: boolean
+
+    /**
+     * Whether to flatten the content item data into the parent content item
+     */
+    flattenData?: boolean
+
+    /**
+     * The content directory to search for content
+     */
+    contentDir?: string
+}
+
+export interface IContentLiteItem {
+    _clId: number
+    slug: string
+    source: string
+    path: string
+    parentPaths: string[]
+    data: any
+    content: string
+    modified: Date
+    words?: Map<string, number>
+}
+
+/**
+ * The raw content item, as it is stored in the database
+ *
+ */
+export type ContentLiteRawItem = [
+    /** The path to the content item */
+    string,
+    /** The modified date of the content item */
+    string,
+    /** The data of the content item */
+    any,
+    /** The content of the content item */
+    string
+]
+
+export interface IContentLiteFindOptions {
+    filterable?: boolean
+    flattenData?: boolean
+}
