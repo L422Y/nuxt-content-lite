08a35b9172e6749f425f84bd4ceebc6a5c2a3c62
Larry W. <l422y@l422y.com>
2024-01-11 01:27:23 -0500

    Add product display and ContentLiteDoc component to basic app.vue

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>basic</div>
+  <div>
+    {{ products }}
+  </div>
+  <ContentLiteDoc :item="awesomePost" />
 </template>
 
 <script setup>
+const content = await useContentLite({flattenData: true})
+const products = await content.find("products")
+const awesomePost = await content.findOne("blog/awesome-post")
+
 </script>
