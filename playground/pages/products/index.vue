699419484129230a9ad5c9c746d465e7cb77fe0a
Larry W. <l422y@l422y.com>
2024-01-10 20:45:41 -0500

    Add products index page with product tiles and related styling

---
+++ playground/pages/products/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="products">
+    <h1>Products</h1>
+    <div class="products--tiles">
+      <ProductTile
+        v-for="product in products"
+        :key="product._clId"
+        :product
+      />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const content = await useContentLite({flattenData: true})
+const products = await content.routeContent()
+</script>
+<style lang="scss" scoped src="~/scss/products.scss"/>
