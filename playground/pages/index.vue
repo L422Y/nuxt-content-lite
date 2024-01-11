9f82dd310d0637f322688e9c8982c16d41bd5960
Larry W. <l422y@l422y.com>
2024-01-10 20:45:34 -0500

    Add index.vue with content fetching and styled pre tag

---
+++ playground/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    Welcome to zombo.com
+    <pre>{{ content.contentData }}</pre>
+  </div>
+</template>
+<script lang="ts" setup>
+const content = await useContentLite({flattenData: true, filterable: true})
+
+</script>
+<style lang="scss" scoped>
+pre {
+  background: #eee;
+  padding: 1rem;
+  max-width: 100%;
+  overflow: scroll;
+  white-space: pre-wrap;
+
+}
+</style>
