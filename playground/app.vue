233ff645a335114a455792b30476d16d55ef4af6
Larry W. <l422y@l422y.com>
2024-01-10 20:44:43 -0500

    Add new app.vue with main navigation and NuxtPage component

---
+++ playground/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <nav>
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      <NuxtLink to="/blog">
+        Blog
+      </NuxtLink>
+      <NuxtLink to="/products">
+        Products
+      </NuxtLink>
+    </nav>
+    <div>
+      <NuxtPage />
+    </div>
+  </main>
+</template>
+<script setup>
+</script>
+<style lang="scss" src="~/scss/app.scss" />
