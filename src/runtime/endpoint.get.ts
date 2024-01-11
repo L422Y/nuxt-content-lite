0b534b83fa82b605818d5f6132263649f4ad9254
Larry W. <l422y@l422y.com>
2024-01-10 22:46:26 -0500

    Remove unnecessary console log in endpoint.get.ts

--- src/runtime/endpoint.get.ts
+++ src/runtime/endpoint.get.ts
@@ -7,7 +7,6 @@ let json: any
 
 export default eventHandler(async (event: H3Event) => {
     if (json === undefined) {
-        console.log(path.resolve("."))
         const jsonPath = path.resolve("./", ".cache/content-lite/content-lite.json")
         json = fs.promises.readFile(jsonPath, "utf-8").then((data) => JSON.parse(data))
     }
