42af8fc3dbe2a6c5ecf851a9bc859cb3008a6cb4
Larry W. <l422y@l422y.com>
2024-01-10 22:45:43 -0500

    Remove unused 'useContentLite' import and 'data' variable in AwesomeComponent.vue

--- playground/components/content/AwesomeComponent.vue
+++ playground/components/content/AwesomeComponent.vue
@@ -2,12 +2,8 @@
   <div class="awesome--component">
     <h1>Awesome Component</h1>
     <p>This is an awesome component!</p>
-    {{ data }}
   </div>
 </template>
 <script lang="ts" setup>
-import { useContentLite } from "../../../src/runtime/composables/useContentLite"
 
-const content = await useContentLite()
-const data = content.contentData
 </script>
