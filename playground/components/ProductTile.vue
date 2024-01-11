b101b85441cb6e1db7c9706e58bae12189dcc518
Larry W. <l422y@l422y.com>
2024-01-10 20:44:51 -0500

    Add ProductTile.vue component for displaying product details

---
+++ playground/components/ProductTile.vue
@@ -0,0 +1,43 @@
+<template>
+  <NuxtLink :to="product.path" class="products--tile">
+    <h2 class="products--tile--title">
+      {{ product.title }}
+    </h2>
+    <p>
+      {{ product.description }}
+    </p>
+    <div class="products--tile--price">
+      {{ product.price }}
+    </div>
+  </NuxtLink>
+</template>
+<script lang="ts" setup>
+defineProps<{
+    product: {
+        title: string
+        description: string
+        price: number
+        path: string
+    }
+}>()
+</script>
+<style lang="scss" scoped>
+.products--tile {
+  padding: 1rem;
+  text-decoration: none;
+  color: #000;
+  border-radius: 0.5rem;
+  background-color: #ccc;
+
+  &--title {
+    font-size: 1.8rem;
+    margin: 0;
+    padding: 0;
+  }
+
+  &--price {
+    font-size: 1.4rem;
+    font-weight: bold;
+  }
+}
+</style>
