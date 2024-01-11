3135f865e617dd090a5941cdadf4bd49075f8487
Larry W. <l422y@l422y.com>
2024-01-11 00:32:05 -0500

    Refactor blog index.vue to use BlogTile component and update posts fetching method

--- playground/pages/blog/index.vue
+++ playground/pages/blog/index.vue
@@ -2,28 +2,16 @@
   <div class="page blog">
     <h1>Blog</h1>
     <div class="blog--tiles">
-      <template
+      <BlogTile
         v-for="post in posts"
         :key="post.title"
-      >
-        <NuxtLink :to="post.path" class="blog--tile">
-          <h2>
-            {{ post.title }}
-          </h2>
-          <time>
-            {{ new Date(post.date).toLocaleDateString() }}
-          </time>
-          <p>
-            {{ post.description }}
-          </p>
-        </NuxtLink>
-      </template>
+        :post="post" :show-author="true"
+      />
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-const posts = ref()
 const content = await useContentLite({flattenData: true})
-posts.value = await content.routeContent()
+const posts = await content.find("blog")
 </script>
-<style lang="scss" scoped src="~/scss/blog.scss" />
+<style lang="scss" scoped src="~/scss/blog.scss"/>
