0fb41ab19947c18edb138182d34cf00b10c5240e
Larry W. <l422y@l422y.com>
2024-01-11 01:45:21 -0500

    Refactor endpoint.get.ts to remove unused imports and parameters

--- src/runtime/endpoint.get.ts
+++ src/runtime/endpoint.get.ts
@@ -1,11 +1,9 @@
-import { H3Event } from "h3"
 import * as path from "path"
 import * as fs from "fs"
-import { createResolver } from "@nuxt/kit"
 
 let json: any
 
-export default eventHandler(async (event: H3Event) => {
+export default eventHandler(async () => {
     if (json === undefined) {
         const jsonPath = path.resolve("./", ".cache/content-lite/content-lite.json")
         json = fs.promises.readFile(jsonPath, "utf-8").then((data) => JSON.parse(data))
