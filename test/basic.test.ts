4cebb6f315618fdc8cc00cb6c3f244f90242c729
Larry W. <l422y@l422y.com>
2024-01-11 01:27:19 -0500

    Add new tests for contentData, vue rendering, and github content layer in basic.test.ts

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,15 +1,40 @@
-import { describe, it, expect } from 'vitest'
-import { fileURLToPath } from 'node:url'
-import { setup, $fetch } from '@nuxt/test-utils/e2e'
+import { describe, expect, it } from "vitest"
+import { fileURLToPath } from "node:url"
+import { $fetch, setup } from "@nuxt/test-utils/e2e"
 
-describe('ssr', async () => {
-  await setup({
-    rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
-  })
+describe("ssr", async () => {
+    await setup({
+        rootDir: fileURLToPath(new URL("./fixtures/basic", import.meta.url)),
+    })
 
-  it('renders the index page', async () => {
-    // Get response to a server-rendered page with `$fetch`.
-    const html = await $fetch('/')
-    expect(html).toContain('<div>basic</div>')
-  })
+    it("renders the index page", async () => {
+        // Get response to a server-rendered page with `$fetch`.
+        const html = await $fetch("/")
+        expect(html).toContain("<div>basic</div>")
+    })
+
+    it("includes the contentData in the initial source", async () => {
+        const html = await $fetch("/")
+        expect(html).toContain("\"$scontentData\"")
+    })
+
+    it("renders the content object with vue", async () => {
+        const html = await $fetch("/")
+        expect(html).toContain("123456788")
+    })
+
+    it("utilizes the github content layer for blog posts", async () => {
+        const html = await $fetch("/")
+        expect(html).toContain("\"blog/awesome-post.md\"")
+    })
+
+    it("renders the awesome blog post", async () => {
+        const html = await $fetch("/")
+        expect(html).toContain("Awesome Post")
+    })
+
+    it("renders the awesome vue component", async () => {
+        const html = await $fetch("/")
+        expect(html).toContain("This is an awesome component!")
+    })
 })
