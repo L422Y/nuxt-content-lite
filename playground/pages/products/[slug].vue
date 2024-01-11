406afb69621d12d2b6740ac1f92f8dccacc183b2
Larry W. <l422y@l422y.com>
2024-01-10 20:45:38 -0500

    Add dynamic product page with error handling in Vue.js

---
+++ playground/pages/products/[slug].vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <template v-if="product">
+      <h1>{{ product.title }}</h1>
+      <h3>${{ product.price }}</h3>
+      <ContentLite :doc="product"/>
+    </template>
+    <template v-else>
+      <h1>Not Found</h1>
+      <p>Sorry, that product doesn't exist.</p>
+    </template>
+  </div>
+</template>
+<script lang="ts" setup>
+const content = await useContentLite()
+const product = await content.findOne(useRoute().path)
+
+const routeContent = await content.routeContent()
+
+</script>
